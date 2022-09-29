ba08cdad69d1ce22a9c3ec0c10ed0d7c7b329e59
Pointerbender <Pointerbender@users.noreply.github.com>
2022-09-29 13:59:15 +0000

    deploy: a3e7b370a461a694ea4405f009ee63d19c84d1d2

--- doc/implementors/digest/trait.Update.js
+++ doc/implementors/digest/trait.Update.js
@@ -1,3 +1,4 @@
-(function() {var implementors = {};
-implementors["digest"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"digest":[],
+"sha1":[["impl Update for <a class=\"struct\" href=\"sha1/struct.Sha1.html\" title=\"struct sha1::Sha1\">Sha1</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- doc/vir/sidebar-items.js
+++ doc/vir/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["common",""],["converter",""],["high",""],["legacy",""],["low",""],["middle",""],["polymorphic",""],["snapshot",""]]});
+window.SIDEBAR_ITEMS = {"macro":[["enum_predicate",""]],"mod":[["common",""],["converter",""],["high",""],["legacy",""],["low",""],["middle",""],["polymorphic",""],["typed",""]]};
